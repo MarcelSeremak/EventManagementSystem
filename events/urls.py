c32586281247ba3899e832ebf22064f27f1d4380
Marcel Seremak <marcelseremak47@gmail.com>
2024-05-22 21:58:55 +0200

    Adding login and registration system to website

--- events/urls.py
+++ events/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.HomepageView.as_view(), name="homepage")
+    path("", views.HomepageView.as_view(), name="homepage"),
+    path('register/', views.RegisterView.as_view(), name='register'),
+    path('login/', views.LoginView.as_view(), name='login'),
+    path('logout/', views.LogoutView.as_view(), name='logout'),
 ]
--- events/views.py
+++ events/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, redirect
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login, logout
 from django.views.generic import View, FormView
+from django.urls import reverse_lazy
 from .forms import Comment
 
 # Create your views here.
@@ -8,3 +11,28 @@ class HomepageView(FormView):
     template_name = "events/homepage.html"
     form_class = Comment
     success_url = "/"
+
+class RegisterView(FormView):
+    template_name = 'events/registration.html'
+    form_class = UserCreationForm
+    success_url = reverse_lazy('homepage')
+
+    def form_valid(self, form):
+        user = form.save()
+        login(self.request, user)
+        return super().form_valid(form)
+
+class LoginView(FormView):
+    template_name = 'events/login.html'
+    form_class = AuthenticationForm
+    success_url = reverse_lazy('homepage')
+
+    def form_valid(self, form):
+        user = form.get_user()
+        login(self.request, user)
+        return super().form_valid(form)
+
+class LogoutView(View):
+    def get(self, request):
+        logout(request)
+        return redirect('login')
