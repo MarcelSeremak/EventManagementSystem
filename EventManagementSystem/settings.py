69297274705fd6f57949623be82e4d333c147193
Marcel Seremak <marcelseremak47@gmail.com>
2024-10-06 19:17:00 +0200

    Update

--- EventManagementSystem/settings.py
+++ EventManagementSystem/settings.py
@@ -1,3 +1,4 @@
+from django.core.management.utils import get_random_secret_key
 """
 Django settings for EventManagementSystem project.
 
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-m#e5zt2n*lh9wbwmmey$lz0yj2ipdl$^3a7cceqzcq)lp^dwpk'
+SECRET_KEY = get_random_secret_key()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
